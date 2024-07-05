3d84a8d57630f07dad4c51832c40b628ecfdf1d9
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-07-04 20:35:35 -0400

    Backend able to receive and return data

--- src/api/stats.py
+++ src/api/stats.py
@@ -14,11 +14,39 @@ router = APIRouter(
 class Input(BaseModel):
     income: int
     state: str
+    gender: str
     weight: int #lbs
     height: int #in
 
 # uvicorn src.api.server:app --host 0.0.0.0 --port 3001
 
-@router.get("/")
-async def root():
-    return {"message": "Testing"}
+@router.post("/")
+async def postStats(stats: Input):
+
+    # Formatting input
+    state = stats.state.lower().capitalize()
+    gender = stats.gender.lower().capitalize()
+    state_gender = state + "_" + gender
+    state += "_Total"
+   
+    national_col = '"medianIncome"'
+    state_col = f'"{state}"'
+    gender_col = f'"{state_gender}"'
+
+    sql = f"""
+        SELECT {national_col} AS medianIncome, {state_col} AS state_median, {gender_col} AS gender_median
+        FROM median
+    """
+    
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(sql)).fetchone()
+        print(result)
+        nationalMedian = result[0]
+        stateMedian = result[1]
+        genderMedian = result[2]
+
+        response = {"national" : stats.income - nationalMedian, 
+                    "state": stats.income - stateMedian,
+                    "gender": stats.income - genderMedian}
+
+    return response
--- src/database.py
+++ src/database.py
@@ -1,9 +1,11 @@
 import os
 import dotenv
 from sqlalchemy import create_engine
+import sqlalchemy
 
 def database_connection_url():
     dotenv.load_dotenv()
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+metadata_obj = sqlalchemy.MetaData()
